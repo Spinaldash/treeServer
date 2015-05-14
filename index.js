78bbca6ab11b7891f1c348df794a114dbedb7254
Spinaldash <blueavenger@gmail.com>
2015-05-14 10:32:19 -0700

    travis encryption

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 
 var Hoek = require('hoek');
 var Server = require('./lib/server');
+var FirebaseTokenGenerator = require("firebase-token-generator");
+var tokenGenerator = new FirebaseTokenGenerator("cUkQnbmXfWWryHSTnlRr4iS58USw0tY9yc3Wd9mF");
+var token = tokenGenerator.createToken({uid: "1", some: "arbitrary", data: "here"}, {admin: true});
 
 Server.init(function(err, server){
   Hoek.assert(!err, err);
   console.log('--------------------------------------------------------------------------------');
   console.log('Hapi:\n', server.info.uri);
   console.log('Environment:\n', server.app.environment);
+  console.log('Firebase Token:', token)
   console.log('--------------------------------------------------------------------------------');
+
 });
