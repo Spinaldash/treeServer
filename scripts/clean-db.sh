8a8fb4e43f1d6f8e1e38d075278236aa25f7d106
Spinaldash <blueavenger@gmail.com>
2015-05-14 13:23:49 -0700

    lives setup

--- scripts/clean-db.sh
+++ scripts/clean-db.sh
@@ -6,4 +6,5 @@ if [ -z "$1" ] ; then
 fi
 
 mongoimport --jsonArray --drop --db $1 --collection users --file ../db/users.json
-mongoimport --jsonArray --drop --db $1 --collection properties --file ../db/trees.json
+mongoimport --jsonArray --drop --db $1 --collection trees --file ../db/trees.json
+mongoimport --jsonArray --drop --db $1 --collection lives --file ../db/lives.json
